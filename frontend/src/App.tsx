f1eaf39d17798c957a5529382579649359a6267b
H4S0 <hasanalic10@gmail.com>
2025-01-07 14:31:34 +0100

    style home layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Home from './Home';
 
 function App() {
   return (
-    <>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24 mt-7">
       <MainNavbar />
       <Routes>
         <Route index element={<Home />}></Route>
@@ -13,7 +13,7 @@ function App() {
 
         {/* Add more routes as needed */}
       </Routes>
-    </>
+    </div>
   );
 }
 
