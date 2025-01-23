142ab094524a76e37d9a3b152b6be3ad07dbccc6
H4S0 <hasanalic10@gmail.com>
2025-01-23 14:11:03 +0100

    add layout,search and add buttons

--- frontend/src/pages/Listings/ListingPage.tsx
+++ frontend/src/pages/Listings/ListingPage.tsx
@@ -1,5 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { IoMenu } from 'react-icons/io5';
+import { FaSearch } from 'react-icons/fa';
+import { MdAddToPhotos } from 'react-icons/md';
+import { BsGridFill } from 'react-icons/bs';
+import { CiGrid2H } from 'react-icons/ci';
 
 const fetchListings = async () => {
   const response = await fetch('http://localhost:4000/listing/getAllListings');
@@ -19,8 +23,23 @@ const ListingPage = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="container mx-auto px-4">
-      <div className="grid grid-cols-4 gap-4 p-4 text-gray-500 font-semibold">
+    <div className="container mx-auto px-4 mt-14">
+      <div className="flex flex-row justify-between">
+        <h2 className="text-3xl">Your listings</h2>
+        <div className="flex flex-row items-center gap-5">
+          <div className="bg-gray-200 p-4 rounded-full">
+            <FaSearch />
+          </div>
+          <div className="bg-gray-200 p-4 rounded-full">
+            <MdAddToPhotos />
+          </div>
+          <div className="bg-gray-200 p-4 rounded-full">
+            <BsGridFill />
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-4 gap-4 p-4 text-gray-500 font-semibold mt-12">
         <p>Listing Name</p>
         <p>Created At</p>
         <p>Location</p>
@@ -30,7 +49,7 @@ const ListingPage = () => {
       {data.map((listing) => (
         <div
           key={listing.id}
-          className="grid grid-cols-4 gap-4 bg-gray-50 rounded-3xl p-4 mb-4 items-center hover:bg-gray-200"
+          className="grid grid-cols-4 gap-4 bg-gray-50 rounded-3xl p-4 mb-4 items-center hover:bg-gray-200 transition"
         >
           <h2 className="font-bold">{listing.listingName}</h2>
 
